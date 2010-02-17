e22342935dabe54bf5dd03453e6c2d5131317ba5
Elliott Fleming <kermin.fleming@intel.com>
2010-02-17 20:14:38 +0000

    Added support for regressions

--- modules/bsv-libraries/bluespec/PLB/test/PLBTester.cpp
+++ modules/bsv-libraries/bluespec/PLB/test/PLBTester.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "asim/provides/connected_application.h"
+#include "asim/rrr/client_stub_PLBDEBUGRRR.h"
 
 using namespace std;
 
@@ -25,6 +26,16 @@ CONNECTED_APPLICATION_CLASS::Init()
 void
 CONNECTED_APPLICATION_CLASS::Main()
 {
+ 
+  // Dump the state of the core every few seconds
+
+  while(1) {
+    sleep(2);
+    OUT_TYPE_getSlaveLoadCommands countSlaveLoadCommands = clientStub->getSlaveLoadCommands(0);
+    OUT_TYPE_getSlaveLoadData countSlaveLoadData = clientStub->getSlaveLoadData(0);
+
+  }
+
 
   exit(0);
 }
