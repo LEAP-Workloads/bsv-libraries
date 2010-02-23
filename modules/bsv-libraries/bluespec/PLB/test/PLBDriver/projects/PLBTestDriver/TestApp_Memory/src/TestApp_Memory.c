d6e284a6a6f07a216d9d87bd9262401946418778
Elliott Fleming <kermin.fleming@intel.com>
2010-02-23 02:56:46 +0000

    added slave driver

--- modules/bsv-libraries/bluespec/PLB/test/PLBDriver/projects/PLBTestDriver/TestApp_Memory/src/TestApp_Memory.c
+++ modules/bsv-libraries/bluespec/PLB/test/PLBDriver/projects/PLBTestDriver/TestApp_Memory/src/TestApp_Memory.c
@@ -71,11 +71,14 @@ int main (void) {
     */
   // print out the data 
 	
-   testMem(0x00000000);
-	testMem(0x00001000);
-   testMem(0x00002000);
-	testMem(0x00003000);
- 
+//   testMem(0x00000000);/
+//	testMem(0x00001000);
+//  testMem(0x00002000);
+//	testMem(0x00003000);
+   testMem(0x00010000);
+   testMem(0x00011000);
+	testMem(0x00013000);
+   testMem(0x00014000);
  
    /*
     * Disable cache and reinitialize it so that other
